e4a8607392f72fcf994dc43f1223c56a958da0c7
Razindra firman <feartohookman@gmail.com>
2018-12-11 19:41:35 +0700

    Update release.js

--- release.js
+++ release.js
@@ -6,8 +6,8 @@
 
 const options = {};
 
-options.ip = '0.0.0.0';
-options.port = parseInt(process.argv[2]);
+//options.ip = '0.0.0.0';
+//options.port = parseInt(process.argv[2]);
 // options.config = { name: 'Total.js' };
 // options.sleep = 3000;
 
